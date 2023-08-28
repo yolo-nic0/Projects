77c5892ea2a23d0f163f5860040d54dafe559d9e
Nico <142056865+yolo-nic0@users.noreply.github.com>
2023-08-29 00:54:18 +0300

    Add files via upload

---
+++ ding/js/index.js
@@ -0,0 +1,16 @@
+// // Get the nav element
+// const nav = document.getElementById("nav");
+
+// // Get the scroll position of the page
+// const scrollY = window.scrollY;
+
+// // Set the width and margin of the nav based on the scroll position
+// if (scrollY > 0) {
+//   nav.style.width = "100%";
+//   nav.style.height = "4rem";
+//   nav.style.margin = "0";
+// } else {
+//   nav.style.width = "calc(100% - 4rem)";
+//   nav.style.margin = "0 2rem";
+// }
+
