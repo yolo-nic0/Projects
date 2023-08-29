7e40666a6aa76361c7249781820d964287cd8ef9
Nico <142056865+yolo-nic0@users.noreply.github.com>
2023-08-29 22:12:32 +0300

    Update index.js

--- ding/js/index.js
+++ ding/js/index.js
@@ -1,16 +1,75 @@
-// // Get the nav element
-// const nav = document.getElementById("nav");
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('over-lay')
 
-// // Get the scroll position of the page
-// const scrollY = window.scrollY;
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
 
-// // Set the width and margin of the nav based on the scroll position
-// if (scrollY > 0) {
-//   nav.style.width = "100%";
-//   nav.style.height = "4rem";
-//   nav.style.margin = "0";
-// } else {
-//   nav.style.width = "calc(100% - 4rem)";
-//   nav.style.margin = "0 2rem";
-// }
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
 
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const rememberCheckbox = document.getElementById("remember");
+const message = document.querySelector(".message");
+
+const login = () => {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username === "" || password === "") {
+    message.innerHTML = "Please enter your username and password";
+    return;
+  }
+
+  // Send the login request to the server
+  fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // The login was successful, redirect the user to the home page
+        window.location.href = "/";
+      } else {
+        message.innerHTML = data.message;
+      }
+    });
+};
+
+document.getElementById("login").addEventListener("click", login);
