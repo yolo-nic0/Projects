8c3fc3af273e15962267aaf6c09eec7ae06d8780
Nico <142056865+yolo-nic0@users.noreply.github.com>
2023-11-12 13:00:35 +0300

    Update commercialWeb.js

--- Biruk's WebDev/commercialWeb.js
+++ Biruk's WebDev/commercialWeb.js
@@ -1,28 +1,8 @@
-window.addEventListener('scroll', handleScroll);
-
-const nav = document.querySelector('.nav');
-
-const handleScroll = () => {
-  if (window.scrollY > 0) {
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-};
-
-let section = document.querySelectorAll('section');
-
-window.onscroll = () => {
-  section.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('showAnimation');
-    }
-    else{
-      sec.classList.remove('showAnimation');
-    }
-  })
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function closeSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
 }
